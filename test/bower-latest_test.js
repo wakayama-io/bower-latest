2298a46611afbd737537ddf42c507d31ed0901d7
Kentaro Wakayama <wakayama.kentaro@gmail.com>
2014-04-30 00:16:37 +0200

    add bower and github request

--- test/bower-latest_test.js
+++ test/bower-latest_test.js
@@ -1,12 +1,30 @@
 'use strict';
 
 var bowerLatest = require('../lib/bower-latest.js');
-var assert = require('should');
+var should = require('should');
 
 describe('bowerLatest', function () {
 
-  it('should be awesome', function () {
-    bowerLatest.awesome().should.equal('awesome');
+  it('should return undefined when no package is found', function (cb) {
+
+    this.timeout(15000);
+
+    // I hope that asdf!@#$%^&*() will never be a name bower compontent
+    bowerLatest('asdf!@#$%^&*()', function(compontent) {
+      should.not.exist(compontent);
+      cb();
+    });
+  });
+
+  it('should return a package', function (cb) {
+
+    this.timeout(15000);
+
+    bowerLatest('backbone', function(compontent) {
+      should(compontent).have.property('name');
+      should(compontent).have.property('version');
+      cb();
+    });
   });
 
 });
