5ad7d443b44313696eee0105b0e693aa9d5c26a6
Kentaro Wakayama <wakayama.kentaro@gmail.com>
2014-04-29 22:57:58 +0200

    inital commit

---
+++ example/simple.js
@@ -0,0 +1,17 @@
+/*
+ * bower-latest
+ * https://github.com/kwakayama/bower-latest
+ *
+ * Copyright (c) 2014 Kentaro Wakayama
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// Nodejs libs.
+// External libs.
+// Internal libs.
+
+var bower-latest = require('bower-latest');
+
+bower-latest.awesome(); // "awesome"
---
+++ test/bower-latest_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var bowerLatest = require('../lib/bower-latest.js');
+var assert = require('should');
+
+describe('bowerLatest', function () {
+
+  it('should be awesome', function () {
+    bowerLatest.awesome().should.equal('awesome');
+  });
+
+});
